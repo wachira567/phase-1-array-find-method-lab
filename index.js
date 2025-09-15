de35eef924b598c014ac1227b1a084e66b5085da
+ <+>
2025-09-15 09:02:17 +0300

    winninggame code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+  // Look through the record to find the first win
+  const winningGame = record.find((game) => game.result === "W");
+
+  if (winningGame) {
+    return winningGame.year;
+  } else {
+    return undefined;
+  }
+}
